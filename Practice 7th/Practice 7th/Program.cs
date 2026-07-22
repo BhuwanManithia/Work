795182c603df72a958eb59341a16bfab7715af85
bmani <bmani@MAANU>
2026-07-22 22:18:06 +0530

    Practice 7th complete

---
+++ Practice 7th/Practice 7th/Program.cs
@@ -0,0 +1,75 @@
+﻿// 1    Print Number from 1 to 10
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+
+//2. Print even numbers from 2 to 20.
+
+//for (int i = 2; i <= 20; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+//3. Print the squares of numbers from 1 to 10.
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i * i);
+//}
+
+
+
+//4. Print the numbers from 10 to 1 in reverse order.
+
+//for (int i = 10; i >= 1; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+//5. Print the first 10 multiples of 5.
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i * 5);
+//}
+
+
+//6. Print all odd numbers between 1 and 30.
+
+//for (int i = 1; i <= 30; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//7. Print the table of 7 (i.e., 7, 14, 21, ..., up to 70).
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine(i * 7);
+//}
+
+
+//8. Print numbers from 50 to 100 with a step of 5.
+
+//for (int i = 50; i <= 100; i += 5)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+//9. Print the sum of numbers from 1 to 100.
+
+
+//int sum = 0;
+
+//for (int i = 1; i <= 100; i++)
+//{
+//    sum += i;
+//}
+
+//Console.WriteLine("Sum = " + sum);
