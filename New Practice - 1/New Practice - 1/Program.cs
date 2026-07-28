cbd46d55c96dcdeada6903bfc23ce085dd3e34d8
bmani <bmani@MAANU>
2026-07-28 11:37:45 +0530

    All Patterns

---
+++ New Practice - 1/New Practice - 1/Program.cs
@@ -0,0 +1,220 @@
+﻿//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = i; j <= 5; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = i; k >= 1; k--)
+//    {
+//        Console.Write(Convert.ToChar(k + 64));
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for(int j = i; j<= 4; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int j = i; j >= 1; j--)
+//    {
+//        Console.Write(j);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j<= i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 5 - i; k++)
+//    {
+//        Console.Write(k);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 5 - i; k++)
+//    {
+//        Console.Write("*");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 5 - i; k++)
+//    {
+//        Console.Write(Convert.ToChar(k + 64));
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+//int l = 1;
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = i; j <= 4; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= i; k++)
+//    {
+
+//        Console.Write(l);
+//        l++;
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= i ; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 5-i; k >= 1;k--)
+//    {
+//        Console.Write(k);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 5 - i; k >= 1; k--)
+//    {
+//        Console.Write(Convert.ToChar(k + 64));
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= 5 - i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= i; k++)
+//    {
+//        Console.Write(k);
+//    }
+//    for (int l = i - 1; l >= 1; l--)
+//    {
+//        Console.Write(l);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= 5 - i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = i; k >= 1; k--)
+//    {
+//        Console.Write(k);
+//    }
+//    for (int l = 2; l <= i; l++)
+//    {
+//        Console.Write(l);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= 5 - i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = i; k >= 1; k--)
+//    {
+//        Console.Write(Convert.ToChar(k + 64));
+//    }
+//    for (int l = 2; l <= i; l++)
+//    {
+//        Console.Write(Convert.ToChar(l + 64));
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+//for (int i = 1; i <= 4; i++)
+//{
+//    for (int j = 1; j <= i; j++)
+//    {
+//        Console.Write(" ");
+//    }
+//    for (int k = 1; k <= 5 - i; k++)
+//    {
+//        Console.Write(k);
+//    }
+//    for (int l = 4 - i; l >= 1; l--)
+//    {
+//        Console.Write(l);
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+
+//for (int i = 1; i <= 5; i++)
+//{
+//    for (int j = 1; j <= 6 - i; j++)
+//    {
+//        Console.Write(j);
+//    }
+//    Console.WriteLine();
+//}
